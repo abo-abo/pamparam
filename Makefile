6cc5b7066f5726292ce9cb4efb659444cce84867
Oleh Krehel <ohwoeowho@gmail.com>
2017-04-15 18:02:25 +0200

    Makefile: Add "update" and "compile"

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
 emacs ?= emacs
+BEMACS = $(emacs) -batch -l elpa.el
 
 all: test
 
 test:
-	$(emacs) -batch -l targets/compile.el -l pamparam.el -l pam-test.el -f ert-run-tests-batch-and-exit
+	$(BEMACS) -batch -l targets/compile.el -l pamparam.el -l pam-test.el -f ert-run-tests-batch-and-exit
 
 compile:
-	$(emacs) -batch -l targets/compile.el
+	$(BEMACS) -batch -l targets/compile.el
+
+update:
+	$(emacs) -batch -l targets/install-deps.el
 
 clean:
 	rm -f *.elc
--- targets/compile.el
+++ targets/compile.el
@@ -1,15 +1,3 @@
-(let ((git-dir (file-name-directory
-                (directory-file-name default-directory))))
-  (add-to-list 'load-path (expand-file-name "lispy" git-dir))
-  (add-to-list 'load-path (expand-file-name "worf" git-dir))
-  (add-to-list 'load-path (expand-file-name "zoutline" git-dir))
-  (add-to-list 'load-path (expand-file-name "ace-link" git-dir))
-  (add-to-list 'load-path (expand-file-name "avy" git-dir))
-  (add-to-list 'load-path (expand-file-name "iedit" git-dir))
-  (add-to-list 'load-path (expand-file-name "swiper" git-dir))
-  (add-to-list 'load-path (expand-file-name "hydra" git-dir)))
-
-
 (setq files '("pamparam.el"))
 (setq byte-compile--use-old-handlers nil)
 (mapc #'byte-compile-file files)
